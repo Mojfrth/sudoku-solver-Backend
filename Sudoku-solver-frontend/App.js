42d4c27a732ec1acbc5ae755e9e34c21ef5b70ed
Mohit <92096086+Mojfrth@users.noreply.github.com>
2023-05-20 19:53:52 +0530

    Add files via upload

---
+++ Sudoku-solver-frontend/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+import Sudoku from "./Sudoku";
+
+function App() {
+  return (
+    <div className='App'>
+      <Sudoku />
+    </div>
+  );
+}
+
+export default App;
+
+
